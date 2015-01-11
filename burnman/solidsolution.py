455ce5a138cde83308800d5bc2355569b5a70fa5
Bob Myhill <myhill.bob@gmail.com>
2015-01-12 00:09:26 +0100

    added composition to attributes for mineral and solidsolution

--- burnman/solidsolution.py
+++ burnman/solidsolution.py
@@ -70,6 +70,18 @@ class SolidSolution(Mineral):
         assert(sum(molar_fractions) < 1.0001)
         self.molar_fractions = molar_fractions 
 
+
+        self.endmember_compositions=[self.endmembers[i][0].composition for i in range(self.n_endmembers)]
+        self.composition=dict()
+        for i, composition in enumerate(self.endmember_compositions):
+            for element in composition:
+                if element not in self.composition:
+                    self.composition[element] = molar_fraction[i]*composition[element]
+                else:
+                    self.composition[element] += molar_fraction[i]*composition[element]
+                
+        
+
     def set_method(self, method):
         for i in range(self.n_endmembers):
             self.endmembers[i][0].set_method(method)
@@ -85,6 +97,7 @@ class SolidSolution(Mineral):
     def set_state(self, pressure, temperature):
         self.pressure=pressure
         self.temperature=temperature
+        
         # Set the state of all the endmembers
         for i in range(self.n_endmembers):
             self.endmembers[i][0].set_state(pressure, temperature)
