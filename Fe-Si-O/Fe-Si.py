3a83ca140e6d1f7bc696d9e21efd0779aac1d853
Bob Myhill <myhill.bob@gmail.com>
2015-04-21 00:43:07 +0200

    Added Si

--- Fe-Si-O/Fe-Si.py
+++ Fe-Si-O/Fe-Si.py
@@ -54,10 +54,10 @@ class FeSi_B20 (Mineral):
             'name': 'FeSi B20',
             'formula': formula,
             'equation_of_state': 'hp_tmt',
-            'H_0': 5050. ,
-            'S_0': 29.90 ,
+            'H_0': -78852. , # Barin
+            'S_0': 44.685 , # Barin
             'V_0': 1.359e-05 ,
-            'Cp': [0.0, 0.0, 0.0, 0.0],
+            'Cp': [38.6770e+01, 0.0217569, -159.151, 0.0060376],
             'a_0': 2.811e-05 ,
             'K_0': 2.057e+11 ,
             'Kprime_0': 4.0 ,
@@ -74,10 +74,10 @@ class FeSi_B2 (Mineral):
             'name': 'FeSi B2',
             'formula': formula,
             'equation_of_state': 'hp_tmt',
-            'H_0': 5050. ,
-            'S_0': 29.90 ,
+            'H_0': -78852. , # to fit
+            'S_0': 44.685 , # to fit
             'V_0': 1.300e-05 ,
-            'Cp': [0.0, 0.0, 0.0, 0.0],
+            'Cp': [38.6770e+01, 0.0217569, -159.151, 0.0060376],
             'a_0': 3.064e-05 ,
             'K_0': 2.199e+11 ,
             'Kprime_0': 4.0 ,
@@ -181,5 +181,198 @@ plt.xlabel("Pressure (GPa)")
 plt.ylabel("Volume (m^3/mol)")
 plt.show()
 
+# 1 bar properties
+# Barin
+S_0_B20=44.685
+G_0_B20=-92175.
+H_0_B20=-78852.
+
+B20.set_state(1.e5, 298.15)
+print G_0_B20, B20.gibbs
+
+FeSi_B20_Cp_data=[]
+for line in open('data/Barin_FeSi_B2_Cp.dat'):
+    content=line.strip().split()
+    if content[0] != '%':
+        FeSi_B20_Cp_data.append(map(float,content))
+
+def fitCp(mineral):
+    def fit(temperatures, a, b, c, d):
+        mineral.params['Cp']=[a, b, c, d]
+        Cp=np.empty_like(temperatures)
+        for i, T in enumerate(temperatures):
+            mineral.set_state(1.e5, T)
+            Cp[i]=mineral.C_p
+        return Cp
+    return fit
 
 
+# Initial guess.
+T_Cp_B20, Cp_B20  = zip(*FeSi_B20_Cp_data)
+guesses=np.array([1, 1, 1,1])
+popt, pcov = optimize.curve_fit(fitCp(B20), np.array(T_Cp_B20), Cp_B20, guesses)
+print popt
+
+temperatures=np.linspace(200., 1700., 100)
+Cps=np.empty_like(temperatures)
+for i, T in enumerate(temperatures):
+    B20.set_state(1.e5, T)
+    Cps[i]=B20.C_p
+
+
+plt.plot( T_Cp_B20, Cp_B20, marker=".", linestyle="None")
+plt.plot( temperatures, Cps, linewidth=1)
+plt.title('Heat Capacity fit')
+plt.xlabel("Temperature (K)")
+plt.ylabel("Heat capacity (J/K/mol)")
+plt.show()
+
+# B20-B2 phase boundary
+class Si_diamond_A4 (Mineral):
+    def __init__(self):
+        formula='Si1.0'
+        formula = dictionarize_formula(formula)
+        self.params = {
+            'name': 'Si A4',
+            'formula': formula,
+            'equation_of_state': 'hp_tmt',
+            'H_0': 0. , # Barin
+            'S_0': 18.820 , # Barin
+            'V_0': 1.20588e-05 , # Hallstedt, 2007
+            'Cp': [22.826, 0.003856857, -353888.416, -0.0596068], # Barin
+            'a_0': 7.757256e-06 , # Fit to Roberts, 1981
+            'K_0': 200.e+9 , # ?
+            'Kprime_0': 4.0 , # ?
+            'Kdprime_0': -4.0/70.e+9 , # ?
+            'T_einstein': 764., # Fit to Roberts, 1981 (would be 516. from 0.8*Tdebye (645 K); see wiki)
+            'n': sum(formula.values()),
+            'molar_mass': formula_mass(formula, atomic_masses)}
+        Mineral.__init__(self)
+
+class Si_fcc_A1 (Mineral):
+    def __init__(self):
+        formula='Si1.0'
+        formula = dictionarize_formula(formula)
+        self.params = {
+            'name': 'Si fcc A1',
+            'formula': formula,
+            'equation_of_state': 'hp_tmt',
+            'H_0': 51000. , # SGTE data
+            'S_0': 18.820 + 21.8 , # Barin, SGTE data
+            'V_0': 1.300e-05 , # ?
+            'Cp': [22.826, 0.003856857, -353888.416, -0.0596068], # Barin
+            'a_0': 3.064e-05 , # ?
+            'K_0': 2.199e+11 , # ?
+            'Kprime_0': 4.0 , # ?
+            'Kdprime_0': -4.0/2.199e+11 , # ?
+            'n': sum(formula.values()),
+            'molar_mass': formula_mass(formula, atomic_masses)}
+        Mineral.__init__(self)
+
+class Si_hcp_A3 (Mineral):
+    def __init__(self):
+        formula='Si1.0'
+        formula = dictionarize_formula(formula)
+        self.params = {
+            'name': 'Si hcp A3',
+            'formula': formula,
+            'equation_of_state': 'hp_tmt',
+            'H_0': 49200., # SGTE data
+            'S_0': 18.820 + 20.8, # Barin, SGTE data
+            'V_0': 1.300e-05 , # ?
+            'Cp': [22.826, 0.003856857, -353888.416, -0.0596068], # Barin
+            'a_0': 3.064e-05 , # ?
+            'K_0': 70.e+9 , # ?
+            'Kprime_0': 4.0 , # ?
+            'Kdprime_0': -4.0/70.e+9 , # ?
+            'n': sum(formula.values()),
+            'molar_mass': formula_mass(formula, atomic_masses)}
+        Mineral.__init__(self)
+
+Si_A4=Si_diamond_A4()
+Si_fcc=Si_fcc_A1()
+Si_hcp=Si_hcp_A3()
+
+Si_A4_Cp_data=[]
+for line in open('data/Barin_Si_A4_Cp.dat'):
+    content=line.strip().split()
+    if content[0] != '%':
+        Si_A4_Cp_data.append(map(float,content))
+
+# Initial guess.
+T_Cp_A4, Cp_A4  = zip(*Si_A4_Cp_data)
+guesses=np.array([1, 1, 1,1])
+popt, pcov = optimize.curve_fit(fitCp(Si_A4), np.array(T_Cp_A4), Cp_A4, guesses)
+print popt
+
+temperatures=np.linspace(200., 1700., 100)
+Cps=np.empty_like(temperatures)
+for i, T in enumerate(temperatures):
+    Si_A4.set_state(1.e5, T)
+    Cps[i]=Si_A4.C_p
+
+
+plt.plot( T_Cp_A4, Cp_A4, marker=".", linestyle="None")
+plt.plot( temperatures, Cps, linewidth=1)
+plt.title('Heat Capacity fit')
+plt.xlabel("Temperature (K)")
+plt.ylabel("Heat capacity (J/K/mol)")
+plt.show()
+
+
+# Thermal expansivity
+Si_A4_a_data=[]
+for line in open('data/Si_thermal_expansion.dat'):
+    content=line.strip().split()
+    if content[0] != '%':
+        Si_A4_a_data.append(map(float,content))
+
+T_a_Si, a_Si = zip(*Si_A4_a_data)
+a_Si=np.array(a_Si)*1.e-6
+
+def fitalpha(mineral):
+    def fit(temperatures, alpha, T_einst):
+        mineral.params['a_0']=alpha
+        mineral.params['T_einstein']=T_einst
+        mineral.set_state(1.e5, 293.)
+        L_293=np.power(mineral.V, 1./3.)
+        alphastar=np.empty_like(temperatures)
+        for i, T in enumerate(temperatures):
+            deltaT=0.1
+            mineral.set_state(1.e5, T-deltaT)
+            L_T0=np.power(mineral.V, 1./3.)
+            mineral.set_state(1.e5, T+deltaT)
+            L_T1=np.power(mineral.V, 1./3.)
+            DeltaL=L_T1 - L_T0
+            DeltaT=2.*deltaT
+            alphastar[i]=(1./L_293)*(DeltaL/DeltaT)
+        return alphastar
+    return fit
+
+
+guesses=np.array([1.e-6, 800.])
+popt, pcov = optimize.curve_fit(fitalpha(Si_A4), np.array(T_a_Si),  a_Si, guesses)
+print popt, pcov
+
+
+temperatures=np.linspace(5., 1700., 100)
+alphastar=np.empty_like(temperatures)
+Si_A4.set_state(1.e5, 293.)
+L_293=np.power(Si_A4.V, 1./3.)
+
+for i, T in enumerate(temperatures):
+    deltaT=0.1
+    Si_A4.set_state(1.e5, T-deltaT)
+    L_T0=np.power(Si_A4.V, 1./3.)
+    Si_A4.set_state(1.e5, T+deltaT)
+    L_T1=np.power(Si_A4.V, 1./3.)
+    DeltaL=L_T1 - L_T0
+    DeltaT=2.*deltaT
+    alphastar[i]=(1./L_293)*(DeltaL/DeltaT)
+
+plt.plot( temperatures, alphastar, linewidth=1)
+plt.plot( T_a_Si, a_Si, marker=".", linestyle="None")
+plt.title('Heat Capacity fit')
+plt.xlabel("Temperature (K)")
+plt.ylabel("Thermal expansivity (10^-6 K^-1)")
+plt.show()
