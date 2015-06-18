1942ffdf7c2b0a4dceb836221689246f821d7cad
Bob Myhill <myhill.bob@gmail.com>
2015-06-18 23:16:41 +0200

    Added more liquids, mixing tests

--- stx_liquids/test_mixing.py
+++ stx_liquids/test_mixing.py
@@ -13,6 +13,18 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 
+fig1 = mpimg.imread('figures/MgO-SiO2_enthalpy_mixing.png')
+plt.subplot(232)
+plt.imshow(fig1, extent=[0, 1, -40000, 10000], aspect='auto')
+
+fig1 = mpimg.imread('figures/MgO-SiO2_entropy_mixing.png')
+plt.subplot(233)
+plt.imshow(fig1, extent=[0, 1, 0, 24], aspect='auto')
+
+fig1 = mpimg.imread('figures/MgO-SiO2_volume_mixing.png')
+plt.subplot(234)
+plt.imshow(fig1, extent=[0, 1, -2.0e-6, 0.4e-6], aspect='auto')
+
 phases = [DKS_2013_liquids.SiO2_liquid(),
          DKS_2013_liquids.MgSiO3_liquid(),
          DKS_2013_liquids.MgSi2O5_liquid(),
@@ -31,35 +43,93 @@ temperature = 3000. # K
 MgO_liq = DKS_2013_liquids.MgO_liquid()
 SiO2_liq = DKS_2013_liquids.SiO2_liquid()
 
-MgO_liq.set_state(pressure, temperature)
-SiO2_liq.set_state(pressure, temperature)
-MgO_gibbs = MgO_liq.gibbs
-SiO2_gibbs = SiO2_liq.gibbs
-
-MgO_V = MgO_liq.V
-SiO2_V = SiO2_liq.V
-
-fSis=[]
-Hexs=[]
-Vexs=[]
-for phase in phases:
-    print phase.params['name']
-
-    nSi = phase.params['formula']['Si']
-    nMg = phase.params['formula']['Mg']
-
-    sum_cations = nSi+nMg
-    fSi=nSi/sum_cations
-
-    phase.set_state(pressure, temperature)
-    Hex = ((phase.gibbs)/sum_cations - (fSi*SiO2_gibbs + (1.-fSi)*MgO_gibbs))/4.
-
-    Vex = (phase.V)/sum_cations - (fSi*SiO2_V + (1.-fSi)*MgO_V)
-    fSis.append(fSi)
-    Hexs.append(Hex)
-    Vexs.append(Vex)
+pts = [[0, 3000],
+       [5, 3000],
+       [10, 3000],
+       [25, 3000],
+       [50, 4000],
+       [75, 5000],
+       [100, 6000],
+       [135, 6000]]
 
 
-plt.plot(fSis, Hexs, marker='o', linestyle='None')
+
+for p, t in pts:
+    print 'Pressure (GPa):', p, ", Temperature (K):", t
+    pressure=p*1.e9
+    temperature=t*1.
+
+    MgO_liq.set_state(pressure, temperature)
+    SiO2_liq.set_state(pressure, temperature)
+    MgO_gibbs = MgO_liq.gibbs
+    SiO2_gibbs = SiO2_liq.gibbs
+
+    MgO_H = MgO_liq.H
+    SiO2_H = SiO2_liq.H
+
+    MgO_S = MgO_liq.S
+    SiO2_S = SiO2_liq.S
+
+    MgO_V = MgO_liq.V
+    SiO2_V = SiO2_liq.V
+
+    MgO_K_T = MgO_liq.K_T
+    SiO2_K_T = SiO2_liq.K_T
+
+    fSis=[]
+    Gexs=[]
+    Hexs=[]
+    Sexs=[]
+    Vexs=[]
+    K_Ts=[]
+    K_Texs=[]
+    for phase in phases:
+        #print phase.params['name']
+        
+        nSi = phase.params['formula']['Si']
+        nMg = phase.params['formula']['Mg']
+        
+        sum_cations = nSi+nMg
+        fSi=nSi/sum_cations
+        
+        phase.set_state(pressure, temperature)
+        Gex = phase.gibbs/sum_cations - (fSi*SiO2_gibbs + (1.-fSi)*MgO_gibbs)       
+        Hex = phase.H/sum_cations - (fSi*SiO2_H + (1.-fSi)*MgO_H)
+
+        Sex = phase.S/sum_cations - (fSi*SiO2_S + (1.-fSi)*MgO_S)
+
+        Vex = phase.V/sum_cations - (fSi*SiO2_V + (1.-fSi)*MgO_V)
+        K_Tex = phase.K_T - (fSi*SiO2_K_T + (1.-fSi)*MgO_K_T)
+        K_T = phase.K_T
+
+        fSis.append(fSi)
+        Gexs.append(Gex)
+        Hexs.append(Hex)
+        Sexs.append(Sex)
+        Vexs.append(Vex)
+        K_Ts.append(K_T)
+        K_Texs.append(K_Tex)
+
+
+    plt.subplot(231)
+    plt.title('Excess Gibbs') 
+    plt.plot(fSis, Gexs, marker='o', linestyle='None', label=str(p)+' GPa, '+str(t)+' K')
+    plt.subplot(232)
+    plt.title('Excess Enthalpies') 
+    plt.plot(fSis, Hexs, marker='o', linestyle='None', label=str(p)+' GPa, '+str(t)+' K')
+    plt.subplot(233)
+    plt.title('Excess Entropies') 
+    plt.plot(fSis, Sexs, marker='o', linestyle='None', label=str(p)+' GPa, '+str(t)+' K')
+    plt.subplot(234)
+    plt.title('Excess Volumes') 
+    plt.plot(fSis, Vexs, marker='o', linestyle='None', label=str(p)+' GPa, '+str(t)+' K')
+    plt.subplot(235)
+    plt.title('Excess Isothermal Bulk Moduli') 
+    plt.plot(fSis, K_Texs, marker='o', linestyle='None', label=str(p)+' GPa, '+str(t)+' K')
+    plt.subplot(236)
+    plt.title('Isothermal Bulk Moduli') 
+    plt.plot(fSis, K_Ts, marker='o', linestyle='None', label=str(p)+' GPa, '+str(t)+' K')
+
+#plt.legend(loc='lower right')
 plt.show()
 
