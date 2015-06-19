ce53679c0f52219a3b5835c9cf44b880a5b043fd
Bob Myhill <myhill.bob@gmail.com>
2015-06-19 23:04:48 +0200

    gibbs endmember adjustments

--- stx_liquids/gibbs_entropy_adjustment.py
+++ stx_liquids/gibbs_entropy_adjustment.py
@@ -12,49 +12,55 @@ from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-'''
-pressure=14.e9 # Pa
-actual_temperature=3120. # K
-temperature = fsolve(find_temperature, 2000., args=(pressure))[0]
-'''
-
-pressure = 14.e9 # Pa
-temperature = 3000. # K
-stv_SLB=SLB_2011.stishovite()
-stv=DKS_2013_solids.stishovite()
-
-stv_SLB.set_state(pressure, temperature)
-stv.set_state(pressure, temperature)
-stv_delta_G=stv.gibbs - stv_SLB.gibbs
-
-print stv_delta_G
-print stv_SLB.gibbs, stv.gibbs-stv_delta_G, stv.gibbs-stv_delta_G-stv_SLB.gibbs
-
-pressure = 14.e9 # Pa
-temperature = 4500. # K
-stv_SLB.set_state(pressure, temperature)
-stv.set_state(pressure, temperature)
-print stv_SLB.gibbs, stv.gibbs-stv_delta_G, \
-    (stv.gibbs-stv_delta_G-stv_SLB.gibbs)/1500.
-
-pressure = 14.e9 # Pa
-temperature = 1500. # K
-stv_SLB.set_state(pressure, temperature)
-stv.set_state(pressure, temperature)
-print stv_SLB.gibbs, stv.gibbs-stv_delta_G, \
-    (stv.gibbs-stv_delta_G-stv_SLB.gibbs)/-1500.
-
-# MELTS
 def find_temperature(temperature, pressure, solid, liquid):
     liquid.set_state(pressure, temperature[0])
     solid.set_state(pressure, temperature[0])
     return solid.gibbs - liquid.gibbs
 
-print 'STISHOVITE'
+
+'''
+Find stishovite delta gibbs and delta entropy
+'''
+
+print 'Corrections to solids'
+print '.....................'
+
+phases=[[SLB_2011.stishovite(), DKS_2013_solids.stishovite()],
+        [SLB_2011.periclase(), DKS_2013_solids.periclase()]]
+
+for SLB_phase, MD_phase in phases:
+    print SLB_phase.params['name']
+    
+    pressure = 14.e9 # Pa
+    temperature_0 = 3000. # K
+
+    SLB_phase.set_state(pressure, temperature_0)
+    MD_phase.set_state(pressure, temperature_0)
+    stv_delta_G=MD_phase.gibbs - SLB_phase.gibbs
+
+    print 'delta_gibbs:', stv_delta_G
+
+    temperatures=[1500, 2000, 2500, 3000, 3500, 4000, 4500]
+
+    for temperature in temperatures:
+        SLB_phase.set_state(pressure, temperature)
+        MD_phase.set_state(pressure, temperature)
+        print temperature, MD_phase.S - SLB_phase.S
+    print ''
+
+print ''
+
+
+print 'Liquid entropy of melting'
+print '.........................'
+
+
 stv=DKS_2013_solids.stishovite()
 SiO2_liq=DKS_2013_liquids.SiO2_liquid()
 pressure = 14.e9 # Pa
 T_melt = fsolve(find_temperature, 5000., args=(pressure, stv, SiO2_liq))[0]
+print 'Stishovite T_melt:', T_melt, 'K'
+
 
 temperatures_SiO2 = np.linspace(1400., T_melt, 101)
 entropy_melting_SiO2 = np.empty_like(temperatures_SiO2)
@@ -63,11 +69,15 @@ for i, temperature in enumerate(temperatures_SiO2):
     stv.set_state(pressure, temperature)
     entropy_melting_SiO2[i] = SiO2_liq.S - stv.S
 
-print 'PERICLASE'
+
 per=DKS_2013_solids.periclase()
 MgO_liq=DKS_2013_liquids.MgO_liquid()
 pressure = 14.e9 # Pa
 T_melt = fsolve(find_temperature, 5000., args=(pressure, per, MgO_liq))[0]
+
+print 'Periclase T_melt:', T_melt, 'K'
+
+
 temperatures_MgO = np.linspace(1400., T_melt, 101)
 entropy_melting_MgO = np.empty_like(temperatures_MgO)
 for i, temperature in enumerate(temperatures_MgO):
