7ac2c5785c28b7fb40425fe92e2bc7dd7ef7b2ee
Bob Myhill <myhill.bob@gmail.com>
2015-07-01 20:40:53 +0200

    Daily update

--- Hunt_Manning_model/SS1985_fo.py
+++ Hunt_Manning_model/SS1985_fo.py
@@ -56,7 +56,7 @@ def excesses_nonideal(X, T, r, K, Wsh, Whs): # X is mole fraction H2O
     partial_excess_H2O+= 2.*Xs*Xs*(1.-Xs)*Wsh - Xs*Xs*(1.-2.*Xs)*Whs 
     return partial_excess_anhydrous_phase, partial_excess_H2O
 
-def activities(X_one_cation, r, K): # X is mole fraction H2O
+def activities(X, r, K): # X is mole fraction H2O
     Xb=X/(X + r*(1.-X)) # eq. 5.3b
     XO=1.-Xb-(0.5 - np.sqrt(0.25 - (K-4.)/K*(Xb-Xb*Xb)))/((K-4.)/K) # eq. 5.3
     XH2O=XO + 2*Xb - 1.0
@@ -64,25 +64,29 @@ def activities(X_one_cation, r, K): # X is mole fraction H2O
     return np.power(XO,r), XH2O, XOH
 
 
-def solve_composition(X_one_cation, T, r, K, Wsh, Whs):
-    n_silicate_per_water = (1.-X_one_cation)/(X_one_cation*n_cations)
-    Xs = 1./(n_silicate_per_water + 1)
+def solve_composition(Xs, T, r, K, Wsh, Whs):
     return dGfo(T) - excesses_nonideal(Xs, T, r, K(T), Wsh(T), Whs(T))[0]
 
 
-
 # 13 GPa, fo
-n_cations=3. # number of cations
-r=4. # Oxygens available for bonding
+r=4./3. # Oxygens available for bonding (one cation basis)
+n_cations = 1.
 Kinf = lambda T: 100000000000.
 K0 = lambda T: 0.00000000001
 K1 = lambda T: np.exp(-(-70000.-15.*T)/(R*T))
 Wsh1 = lambda T: 0
 
-K = lambda T: 100000000000. # np.exp(-(-50000-15.*(T - 2000.))/(R*T))
+K = lambda T: 1000000000000. # np.exp(-(-50000-15.*(T - 2000.))/(R*T))
 Wsh = lambda T: 00000.
 
-Whs = lambda T: -500000.
+Whs = lambda T: 00000.
+
+'''
+Tbr = 1.
+X = 5./10.
+print ((1-X)*excesses_nonideal(X, Tbr, r, K(Tbr), Wsh(Tbr), Whs(Tbr))[0] + X*excesses_nonideal(X, Tbr, r, K(Tbr), Wsh(Tbr), Whs(Tbr))[1])
+exit()
+'''
 
 fo=SLB_2011.forsterite()
 Mg2SiO4_liq=DKS_2013_liquids_tweaked.Mg2SiO4_liquid()
@@ -92,25 +96,25 @@ print fsolve(find_eqm_temperature, 2000., args=(13.e9, fo, Mg2SiO4_liq, 1.))
 def dGfo(temperature):
     fo.set_state(13.e9, temperature)
     Mg2SiO4_liq.set_state(13.e9, temperature)
-    return (fo.gibbs - Mg2SiO4_liq.gibbs)
+    return (fo.gibbs - Mg2SiO4_liq.gibbs)/3.
 
-'''
+
 compositions=np.linspace(0.0001, 0.99, 101)
 Gex=np.empty_like(compositions)
 Gex_2=np.empty_like(compositions)
 for i, X in enumerate(compositions):
-    Tbr = 1500.
+    Tbr = 1000.
     Gex[i]=(1-X)*excesses_nonideal(X, Tbr, r, K(Tbr), Wsh(Tbr), Whs(Tbr))[0] + X*excesses_nonideal(X, Tbr, r, K(Tbr), Wsh(Tbr), Whs(Tbr))[1]
-    Tmelt = 3000.
+    Tmelt = 2000.
     Gex_2[i]=(1-X)*excesses_nonideal(X, Tmelt, r, K(Tmelt), Wsh(Tmelt), Whs(Tmelt))[0] + X*excesses_nonideal(X, Tmelt, r, K(Tmelt), Wsh(Tmelt), Whs(Tmelt))[1]
 
-plt.plot( compositions, Gex, '-', linewidth=2., label='model at 1500 K')
-plt.plot( compositions, Gex_2, '-', linewidth=2., label='model at 3000 K')
+plt.plot( compositions, Gex, '-', linewidth=2., label='model at 1000 K')
+plt.plot( compositions, Gex_2, '-', linewidth=2., label='model at 2000 K')
 plt.ylabel("Excess Gibbs (J/mol)")
 plt.xlabel("X")
 plt.legend(loc='lower left')
 plt.show()
-'''
+
 
 
 fn0=lambda T: 0.
@@ -122,7 +126,7 @@ compositionsinf=np.empty_like(temperatures)
 temperatures_fo=np.linspace(1600., 3000., 101)
 compositions_fo=np.empty_like(temperatures_fo)
 
-'''
+
 for i, T in enumerate(temperatures):
     compositions0[i]=fsolve(solve_composition, 0.001, args=(T, r, K0, fn0, fn0))
     compositionsinf[i]=fsolve(solve_composition, 0.001, args=(T, r, Kinf, fn0, fn0))
@@ -137,7 +141,7 @@ plt.plot( compositions_fo, temperatures_fo, linewidth=1, label='fo')
 #plt.plot( compositions, temperatures, linewidth=1, label='K=K(T)')
 plt.plot( compositionsinf, temperatures, linewidth=1, label='K=inf')
 plt.plot( compositions0, temperatures, linewidth=1, label='K=0')
-'''
+
 
 ###################
 # CALCULATE LIQUIDUS SPLINE
@@ -189,7 +193,7 @@ compositions = np.linspace(0., 0.6, 101)
 activities = np.empty_like(temperatures)
 for i, composition in enumerate(compositions):
     temperature = spline_PW1993(composition)+273.15
-    activities[i] =  np.exp( dGfo(temperature)/3. / (constants.gas_constant*temperature))
+    activities[i] =  np.exp( dGfo(temperature) / (constants.gas_constant*temperature))
 
    
 plt.plot(compositions, activities)
