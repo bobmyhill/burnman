d1c698d9e8effaf0354320021119a5d28539a99e
Bob Myhill <myhill.bob@gmail.com>
2015-07-25 22:45:50 +0200

    Added ol_opx_Fe4O5 equilibrium draft

--- fe4o5/ol_opx_fe4o5.py
+++ fe4o5/ol_opx_fe4o5.py
@@ -16,77 +16,25 @@ import scipy.optimize as optimize
 
 from mineral_models import *
 
+ol = olivine()
+wad = wadsleyite()
+rw=ringwoodite()
 
-# Variables should be entropy, thermal expansion (although we have essentially no constraints on these), enthalpy and bulk modulus
+fm45 = MgFeFe2O5()
 
-class enstatite (Mineral):
-    def __init__(self):
-       formula='Mg2.0Si2.0O6.0'
-       formula = dictionarize_formula(formula)
-       self.params = {
-            'name': 'en',
-            'formula': formula,
-            'equation_of_state': 'hp_tmt',
-            'H_0': -3090220.0 ,
-            'S_0': 132.5 ,
-            'V_0': 6.262e-05 ,
-            'Cp': [356.2, -0.00299, -596900.0, -3185.3] ,
-            'a_0': 2.27e-05 ,
-            'K_0': 1.059e+11 ,
-            'Kprime_0': 8.65 ,
-            'Kdprime_0': -8.2e-11 ,
-            'n': sum(formula.values()),
-            'molar_mass': formula_mass(formula, atomic_masses)}
-
-class ferrosilite (Mineral):
-    def __init__(self):
-       formula='Fe2.0Si2.0O6.0'
-       formula = dictionarize_formula(formula)
-       self.params = {
-            'name': 'fs',
-            'formula': formula,
-            'equation_of_state': 'hp_tmt',
-            'H_0': -2388710.0 ,
-            'S_0': 189.9 ,
-            'V_0': 6.592e-05 ,
-            'Cp': [398.7, -0.006579, 1290100.0, -4058.0] ,
-            'a_0': 3.26e-05 ,
-            'K_0': 1.01e+11 ,
-            'Kprime_0': 4.08 ,
-            'Kdprime_0': -4e-11 ,
-            'n': sum(formula.values()),
-            'molar_mass': formula_mass(formula, atomic_masses)}
-
-DeltaH_fm=-6950.
-class ordered_fm_opx (Mineral):
-    def __init__(self):
-       formula='Fe2.0Si2.0O6.0'
-       formula = dictionarize_formula(formula)
-       self.params = {
-            'name': 'fm',
-            'formula': formula,
-            'equation_of_state': 'hp_tmt',
-            'H_0': (-2388710.0-3090220.0)/2.+DeltaH_fm ,
-            'S_0': (132.5+189.9)/2. ,
-            'V_0': (6.592e-05+6.262e-05)/2. ,
-            'Cp': [(398.7+356.2)/2., (-0.006579-0.00299)/2., (1290100.0-596900.0)/2., (-4058.0-3185.3)/2.] ,
-            'a_0': (3.26e-05+2.27e-05)/2. ,
-            'K_0': (1.01e+11+1.059e+11)/2. ,
-            'Kprime_0': (4.08+8.65)/2. ,
-            'Kdprime_0': -1.*(4.08+8.65)/(1.01e+11+1.059e+11) ,
-            'n': sum(formula.values()),
-            'molar_mass': formula_mass(formula, atomic_masses)}
-
-
-
-
-en=enstatite()
-fs=ferrosilite()
+en = minerals.HP_2011_ds62.en()
+fs = minerals.HP_2011_ds62.fs()
 fm=ordered_fm_opx()
 
-en.set_method('hp_tmt')
-fs.set_method('hp_tmt')
-fm.set_method('hp_tmt')
+# EMOD minerals
+mag = minerals.HP_2011_ds62.mag()
+fo = minerals.HP_2011_ds62.fo()
+diam = minerals.HP_2011_ds62.diam()
+
+# QFM minerals
+q= minerals.HP_2011_ds62.q()
+fa = minerals.HP_2011_ds62.fa()
+mt = minerals.HP_2011_ds62.mt()
 
 P=0.1e9
 T=1273.
@@ -102,7 +50,7 @@ class orthopyroxene(burnman.SolidSolution):
         # Name
         self.name='Fe-Mg orthopyroxene'
 
-        base_material = [[minerals.HP_2011_ds62.en(), '[Mg][Mg]Si2O6'],[minerals.HP_2011_ds62.fs(), '[Fe][Fe]Si2O6'],[ordered_fm_opx(), '[Mg][Fe]Si2O6']]
+        base_material = [[burnman.minerals.HP_2011_ds62.en(), '[Mg][Mg]Si2O6'],[burnman.minerals.HP_2011_ds62.fs(), '[Fe][Fe]Si2O6'],[ordered_fm_opx(), '[Mg][Fe]Si2O6']]
 
         # Interaction parameters
         enthalpy_interaction=[[6.8e3, 4.5e3],[4.5e3]]
@@ -113,12 +61,16 @@ class orthopyroxene(burnman.SolidSolution):
 
 opx=orthopyroxene()
 
+################################
+# Check order parameter finder #
+################################
 
 def opx_composition(arg, X_Mg, P, T):
     Q=arg[0]
     opx.set_composition([X_Mg-0.5*Q, 1-X_Mg-0.5*Q, Q])
     opx.set_state(P,T)
-    return opx.gibbs
+    return 0.5*(opx.partial_gibbs[0] + opx.partial_gibbs[1]) - opx.partial_gibbs[2] 
+
 
 P=1.e9
 T=1473.15
@@ -126,15 +78,11 @@ X_Mgs=np.linspace(0.0,1.0,101)
 G=np.empty_like(X_Mgs)
 Q=np.empty_like(X_Mgs)
 for idx, X_Mg in enumerate(X_Mgs):
-    optimize.minimize(opx_composition, [-0.01], method='nelder-mead', args=(X_Mg, P, T))
+    optimize.fsolve(opx_composition, [0.2], args=(X_Mg, P, T))
     G[idx]=opx.gibbs
-    Q[idx]=opx.molar_fraction[2]
+    Q[idx]=opx.molar_fractions[2]
 
 plt.plot( X_Mgs, G, '-', linewidth=3., label='Gibbs opx')
-
-X_Mg=0.5
-optimize.minimize(opx_composition, [-0.01], method='nelder-mead', args=(X_Mg, P, T))
-print opx.partial_gibbs, opx.gibbs, (opx.partial_gibbs[0]+opx.partial_gibbs[1])/2.
 
 plt.title('')
 plt.xlabel("X_Mg opx")
@@ -143,62 +91,74 @@ plt.legend(loc='lower right')
 plt.show()
 
 
-# (Mg,Fe)2Fe2O5 - ol/wad/rw - opx/hpx diagram
-# i.e. 
-# 2(Mg,Fe)Fe3O5
-# (Mg,Fe)2SiO4
-# (Mg,Fe)2Si2O6
+#################################
+# Find ol-opx-Fe4O5 equilibrium #
+#################################
+
+# Constraints
+# ol composition
+
+# Unknowns
+# opx order parameter
+# opx composition
+# Fe4O5 composition
+
+# Reactions
+# opx (order parameter)
+# Fe-Mg exchange (ol-opx)
+# Fe-Mg exchange (ol-Fe4O5)
+
+def ol_opx_fm45_equilibrium(args, X_Mg_ol, P, T):
+    Q, X_Mg_opx, X_Mg_Fe4O5 = args
+    ol.set_composition([X_Mg_ol, 1.-X_Mg_ol])
+    opx.set_composition([X_Mg_opx-0.5*Q, 1-X_Mg_opx-0.5*Q, Q])
+    fm45.set_composition([X_Mg_Fe4O5, 1.-X_Mg_Fe4O5])
+
+    ol.set_state(P, T)
+    opx.set_state(P, T)
+    fm45.set_state(P, T)
+
+    equations = [ 0.5*(opx.partial_gibbs[0] + opx.partial_gibbs[1]) - opx.partial_gibbs[2],
+                  ( ol.partial_gibbs[0] + opx.partial_gibbs[1] ) - ( ol.partial_gibbs[1] + opx.partial_gibbs[0] ), 
+                  ( ol.partial_gibbs[0] + 2.*fm45.partial_gibbs[1] ) - ( ol.partial_gibbs[1] + 2.*fm45.partial_gibbs[0] ) ]
+    
+    return equations
 
 
-#bulk composition = (Mg,Fe)6 Fe6 Si3 O20
-# X_Mg_bulk = sum(X_Mg)/3.
+# Define EMOD oxygen buffer
+# enstatite, magnesite, olivine, diamond
+EMOD = [en, mag, fo, diam]
+QFM = [q, fa, mt]
 
-def foo_compositions(arg, ol_polymorph, bulk_XMg, P, T):
-    # args are X_Mg_Fe4O5, X_Mg_ol_polymorph, Q_opx
-    X_Mg_Fe4O5=float(arg[0])
-    X_Mg_ol_polymorph=float(arg[1])
-    X_Mg_opx=3.*bulk_XMg -1.0*X_Mg_Fe4O5 -1.0*X_Mg_ol_polymorph
-    Q_opx=float(arg[2])
-
-    fm45.set_composition([X_Mg_Fe4O5, 1.0-X_Mg_Fe4O5])
-    ol_polymorph.set_composition([X_Mg_ol_polymorph, 1.0-X_Mg_ol_polymorph])
-
-
-    p_en=X_Mg_opx - Q_opx/2.
-    p_fm=Q_opx
-    p_fs=1. - p_en - p_fm
-
-    opx.set_composition([p_en, p_fs, p_fm])
-
-    fm45.set_state(P,T)
-    ol_polymorph.set_state(P,T)
-    opx.set_state(P,T)
-
-    return 2.*fm45.gibbs + ol_polymorph.gibbs + opx.gibbs
-
-
-P=10.5e9
-T=1373.
 O2=minerals.HP_2011_fluids.O2()
 O2.set_method('cork')
+
+pressures = np.linspace(8.e9, 16.e9, 9)
+fO2_ol_opx_fm45 = np.empty_like(pressures)
+fO2_EMOD = np.empty_like(pressures)
+fO2_QFM = np.empty_like(pressures)
+
+T = 1673.15
 O2.set_state(1.e5,T)
+X_Mg_ol = 0.9
 
-Re=minerals.Metal_Metal_oxides.Re()
-ReO2=minerals.Metal_Metal_oxides.ReO2()
-Re.set_method('hp_tmt')
-ReO2.set_method('hp_tmt')
+for i, P in enumerate(pressures):
+    print optimize.fsolve(ol_opx_fm45_equilibrium, [0.0, 0.94, 0.7], args=(X_Mg_ol, P, T))
+    fO2_ol_opx_fm45[i] =  np.log10(fugacity(O2, [ol, opx, fm45]))
 
-bulk_XMgs=np.linspace(0.01, 0.8, 11)
-for bulk_XMg in bulk_XMgs:
-    ol_polymorph=ol
-    optimize.minimize(foo_compositions, [0.00,0.0,-0.01], method='nelder-mead', args=(ol_polymorph, bulk_XMg, P, T))
+    for mineral in EMOD:
+        mineral.set_state(P, T)
+    for mineral in QFM:
+        mineral.set_state(P, T)
 
-    assemblage=[ol, opx, fm45]
-    #print ol.partial_gibbs, opx.partial_gibbs
-    print ol.molar_fraction[0], opx.molar_fraction[0] + 0.5*opx.molar_fraction[2], fm45.molar_fraction[0], np.log10(fugacity(dictionarize_formula('O2'), O2, assemblage))
+    fO2_EMOD[i] = np.log10(fugacity(O2, EMOD))
+    fO2_QFM[i] = np.log10(fugacity(O2, QFM))
 
-    Re.set_state(P,T)
-    ReO2.set_state(P,T)
-    print np.log10(fugacity(dictionarize_formula('O2'), O2, [Re, ReO2]))
-# find oxygen fugacity
- 
+
+
+plt.plot(pressures/1.e9, fO2_ol_opx_fm45, label='ol-opx-Fe4O5')
+plt.plot(pressures/1.e9, fO2_EMOD, label='EMOD')
+plt.plot(pressures/1.e9, fO2_QFM, label='QFM')
+
+plt.legend(loc='lower left')
+plt.show()
