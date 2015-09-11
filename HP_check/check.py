80a768dd31fef3376e5c565b85b11d1edfd65644
Bob Myhill <myhill.bob@gmail.com>
2015-09-11 15:58:28 +0200

    Added room temperature EoS check

--- HP_check/check.py
+++ HP_check/check.py
@@ -4,8 +4,42 @@ sys.path.insert(1,os.path.abspath('..'))
 import numpy as np
 import burnman
 
+coe = burnman.minerals.HP_2011_ds62.coe()
 stv = burnman.minerals.HP_2011_ds62.stv()
+stv_SLB = burnman.minerals.SLB_2011.stishovite()
 gr = burnman.minerals.HP_2011_ds62.gr()
+
+# Better fit to Ito et al. (1974) with these parameters
+#stv.params['a_0'] = 1.4e-5
+#stv.params['V_0'] = 1.4006e-5
+
+coe.set_state(0.001e8, 2000.+273.15)
+stv.set_state(0.001e8, 2000.+273.15)
+print coe.gibbs - -1162.09e3, 'CHECK ZERO'
+print stv.gibbs - -1094.73e3, 'CHECK ZERO'
+
+coe.set_state(100.e8, 2000.+273.15)
+stv.set_state(100.e8, 2000.+273.15)
+print coe.gibbs - -959.30e3, 'CHECK ZERO'
+print stv.gibbs - -949.73e3, 'CHECK ZERO'
+
+print 'Checking volume'
+dP = 100.
+stv.set_state(1.e5, 2000.+273.15)
+G0 = stv.gibbs
+V0 = stv.V
+stv.set_state(1.e5+dP, 2000.+273.15)
+G1 = stv.gibbs
+print V0, (G1-G0)/dP
+
+print 'Checking thermal expansion'
+dT = 0.1
+stv.set_state(1.e5, 298.15)
+V0 = stv.V
+stv.set_state(1.e5, 298.15+dT)
+V1 = stv.V
+print 1/V0*(V1-V0)/dT, stv.params['a_0']
+
 
 f = open('burnman_stv_volumes.dat', 'w')
 temperatures = np.linspace(50., 900., 101)
@@ -14,6 +48,16 @@ for i, T in enumerate(temperatures):
     stv.set_state(1.e5, T)
     stv_volumes[i] = stv.V*1.e5
     f.write(str(T)+' '+str(stv_volumes[i])+'\n')
+
+f.close()
+
+f = open('burnman_stv_volumes_298K.dat', 'w')
+pressures = np.linspace(1.e5, 230.e9, 101)
+stv_volumes = np.empty_like(pressures)
+for i, P in enumerate(pressures):
+    stv.set_state(P, 298.)
+    stv_volumes[i] = stv.V*1.e5
+    f.write(str(P/1.e9)+' '+str(stv_volumes[i])+'\n')
 
 f.close()
 
--- HP_check/plot_figures.bash
+++ HP_check/plot_figures.bash
@@ -4,9 +4,19 @@
 python ./check.py
 
 base="stv"
-psbasemap -JX15/10 -R0/900/1.390/1.415 -B200f100:"Temperature (K)":/0.005f0.001:"Volume (kJ/kbar)":SWen -K -P > ${base}.ps
+psbasemap -JX15/10 -R0/900/1.395/1.420 -B200f100:"Temperature (K)":/0.005f0.001:"Volume (kJ/kbar)":SWen -K -P > ${base}.ps
 psxy data/stv_volumes.dat -J -R -O -K -W1,red >> ${base}.ps
 psxy burnman_stv_volumes.dat  -J -R -O -W1,black >> ${base}.ps
+
+ps2epsi ${base}.ps
+epstopdf ${base}.epsi
+rm ${base}.ps ${base}.epsi
+evince ${base}.pdf
+
+base="stv_298K"
+psbasemap -JX15/10 -R0./230/0.96/1.42 -B50f10:"Pressure (GPa)":/0.05f0.1:"Volume (kJ/kbar)":SWen -K -P > ${base}.ps
+psxy data/stv_volumes_298K.dat -J -R -O -K -W1,red >> ${base}.ps
+psxy burnman_stv_volumes_298K.dat  -J -R -O -W1,black >> ${base}.ps
 
 ps2epsi ${base}.ps
 epstopdf ${base}.epsi
