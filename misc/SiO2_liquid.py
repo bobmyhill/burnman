965b563f2de772fe7bd98528f668cc31a7866aa6
Bob Myhill <myhill.bob@gmail.com>
2017-03-14 12:39:34 +0000

    Added PVT] plots

--- misc/SiO2_liquid.py
+++ misc/SiO2_liquid.py
@@ -17,9 +17,9 @@ import burnman
 
 dS = 0.
 dV = 0.
-SiO2_liquid = burnman.CombinedMineral([burnman.SesameMineral('SiO2_sesame_301', 'SiO2')],
-                                      [1.0],
-                                      [0., dS, dV])
+SiO2_liquid = burnman.SesameMineral('SiO2_sesame_301', 'SiO2')
+
+
 
 '''
 # Check gridding
@@ -28,9 +28,56 @@ print(SiO2_liquid.rho, '4480')
 exit()
 '''
 
+
+eVoverK = 8.621738e-5
+print(3073.*8.621738e-5)
+
+pressures = np.linspace(100.e9, 100000.e9, 101) 
+for eV in [100., 50., 20., 10., 5]:
+    T = eV/eVoverK
+    print(T)
+    temperatures = [T] * len(pressures)
+    densities = SiO2_liquid.evaluate(['rho'], pressures, temperatures)[0]
+    plt.plot(densities/1000., np.log10(pressures/1.e9))
+plt.xlim(5, 16)
+plt.ylim(2, 5)    
+plt.show()
+
+pressures = np.linspace(100.e9, 3000.e9, 101) 
+for eV in [6.89, 5.17, 3.45, 1.72, 0.86]:
+    T = eV/eVoverK
+    print(T)
+    temperatures = [T] * len(pressures)
+    densities = SiO2_liquid.evaluate(['rho'], pressures, temperatures)[0]
+    plt.plot(densities/1000., pressures/1.e9)
+plt.xlim(5, 11.5)
+plt.ylim(0, 3000.)    
+plt.show()
+
+
+pressures = np.linspace(1.e9, 100.e9, 101) 
+for T in np.linspace(3000., 10000., 8):
+    print(T)
+    temperatures = [T] * len(pressures)
+    densities = SiO2_liquid.evaluate(['rho'], pressures, temperatures)[0]
+    plt.plot(densities/1000., pressures/1.e9, label='{0:.0f} K'.format(T))
+plt.legend(loc='lower right')
+plt.xlabel('Density (g/cc)')
+plt.ylabel('P (GPa)')
+plt.xlim(3, 5.)
+plt.ylim(0, 100.)    
+plt.show()
+    
+exit()
+
 stv = burnman.minerals.HP_2011_ds62.stv()
 coe = burnman.minerals.HP_2011_ds62.coe()
 coe = burnman.minerals.SLB_2011.coesite()
+
+SiO2_liquid.set_state(13.7e9, 3073.)
+coe.set_state(13.7e9, 3073.)
+print('Density SiO2_liquid, coesite')
+print(SiO2_liquid.rho, coe.rho)
 
 '''
 P = 13.7e9
@@ -68,6 +115,10 @@ plt.show()
 exit()
 '''
 
+fig = plt.figure()
+ax_P = fig.add_subplot(1, 2, 1)
+ax_T = fig.add_subplot(1, 2, 2)
+
 
 prop = 'rho'
 
@@ -77,14 +128,13 @@ for temperature in [3073.]:
     values = SiO2_liquid.evaluate([prop], pressures, temperatures)[0]
     coe_values = coe.evaluate([prop], pressures, temperatures)[0]
     
-    plt.plot(pressures/1.e9, values, label='liq @ {0} K'.format(temperature))
-    plt.plot(pressures/1.e9, coe_values, linestyle=':', label='coe @ {0} K'.format(temperature))
+    ax_P.plot(pressures/1.e9, values, label='liq @ {0} K'.format(temperature))
+    ax_P.plot(pressures/1.e9, coe_values, linestyle=':', label='coe @ {0} K'.format(temperature))
 
     
-plt.legend(loc='upper left')
-plt.xlabel('Pressures (GPa)')
-plt.ylabel(prop)
-plt.show()
+ax_P.legend(loc='upper left')
+ax_P.set_xlabel('Pressures (GPa)')
+ax_P.set_ylabel(prop)
 
 temperatures = np.linspace(2000., 4000., 101)
 for pressure in [13.7e9]:
@@ -92,9 +142,10 @@ for pressure in [13.7e9]:
     values = SiO2_liquid.evaluate([prop], pressures, temperatures)[0]
     coe_values = coe.evaluate([prop], pressures, temperatures)[0]
     
-    plt.plot(temperatures, values, label='liq @ {0} GPa'.format(pressure/1.e9))
-    plt.plot(temperatures, coe_values, linestyle=':', label='coe @ {0} GPa'.format(pressure/1.e9))
-plt.legend(loc='upper left')
-plt.xlabel('Temperatures (K)')
-plt.ylabel(prop)
+    ax_T.plot(temperatures, values, label='liq @ {0} GPa'.format(pressure/1.e9))
+    ax_T.plot(temperatures, coe_values, linestyle=':', label='coe @ {0} GPa'.format(pressure/1.e9))
+ax_T.legend(loc='upper right')
+ax_T.set_xlabel('Temperatures (K)')
+ax_T.set_ylabel(prop)
+
 plt.show()
