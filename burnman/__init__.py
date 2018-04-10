50546888e4705ab31558d060e02ae3f33474f4e2
Bob Myhill <myhill.bob@gmail.com>
2018-04-10 23:05:53 +0100

    preparation for equilibrium functions

--- burnman/__init__.py
+++ burnman/__init__.py
@@ -207,3 +207,4 @@ from . import nonlinear_fitting
 from . import nonlinear_solvers
 from . import eos_fitting
 from .partitioning import calculate_nakajima_fp_pv_partition_coefficient
+from .equilibrate import equilibrate
