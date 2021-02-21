9824ff77af2f9080c8e0379bf0f30af02dab5172
Bob Myhill <myhill.bob@gmail.com>
2021-02-21 16:49:01 +0000

    added endmember EoSes

--- contrib/water_filter/model_parameters.py
+++ contrib/water_filter/model_parameters.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 R = 8.31446  # gas constant
 
 liq_sp = {'H_poly': -94000.,
@@ -56,16 +58,89 @@ olwad = {'Delta_E': ol['E'] - wad['E'],
          'Delta_S': ol['S'] - wad['S'],
          'Delta_V': ol['V'] - wad['V'],
          'halfPint0': 0.976e9,  # estimated from SLB2011 Figure
-         'dhalfPintdT': -4.05e5} # estimated from SLB2011 Figure
+         'dhalfPintdT': -4.05e5}  # estimated from SLB2011 Figure
 
 wadring = {'Delta_E': wad['E'] - ring['E'],
            'Delta_S': wad['S'] - ring['S'],
            'Delta_V': wad['V'] - ring['V'],
            'halfPint0': 2.286e9,   # estimated from SLB2011 Figure
-           'dhalfPintdT': -8.85e5} # estimated from SLB2011 Figure
+           'dhalfPintdT': -8.85e5}  # estimated from SLB2011 Figure
 
 ringlm = {'Delta_E': ring['E'] - lm['E'],
           'Delta_S': ring['S'] - lm['S'],
           'Delta_V': ring['V'] - lm['V'],
           'halfPint0': 0.1e9,  # Something small, could increase this to simulate garnet decomposition.
           'dhalfPintdT': 0.}  # 0.
+
+# Endmember equation of state parameters
+mg_wadsleyite_params = {'name': 'Mg wadsleyite',
+                        'equation_of_state': 'mod_hp_tmt',
+                        'T_0': 298.15,
+                        'Pref': 17500000000.0,
+                        'V_0': 4.073795492729634e-05,
+                        'K_0': 157218159278.60214,
+                        'Kprime_0': 4.747568746830502,
+                        'Kdprime_0': -4.235205844957051e-11,
+                        'a_0': 1.0724967177696426e-05,
+                        'T_einstein': 1142.5572613375452,
+                        'H_Pref': -1324992.8391624494,
+                        'S_Pref': 81.1060010156774,
+                        'Cp_Pref': np.array([1.49312734e+02, 9.66981406e-03,
+                                             -1.06823762e+07, 7.20496606e+02]),
+                        'molar_mass': 0.14069310000000002,
+                        'n': 7.0,
+                        'formula': {'Mg': 2.0, 'Si': 1.0, 'O': 4.0}}
+
+fe_wadsleyite_params = {'name': 'Fe wadsleyite',
+                        'equation_of_state': 'mod_hp_tmt',
+                        'T_0': 298.15,
+                        'Pref': 17500000000.0,
+                        'V_0': 4.302047981460708e-05,
+                        'K_0': 157940753600.1616,
+                        'Kprime_0': 4.716105207436653,
+                        'Kdprime_0': -4.062575119500919e-11,
+                        'a_0': 1.221163276444518e-05,
+                        'T_einstein': 1029.050289141727,
+                        'H_Pref': -616069.0953829046,
+                        'S_Pref': 133.85124728860626,
+                        'Cp_Pref': np.array([1.51964362e+02, 8.80493315e-03,
+                                             -7.74825617e+06, 6.51972863e+02]),
+                        'molar_mass': 0.20377309999999998,
+                        'n': 7.0,
+                        'formula': {'Fe': 2.0, 'Si': 1.0, 'O': 4.0}}
+
+majorite_params = {'name': 'Mg majorite',
+                   'equation_of_state': 'mod_hp_tmt',
+                   'T_0': 298.15,
+                   'Pref': 17500000000.0,
+                   'V_0': 2.8648145222703425e-05,
+                   'K_0': 160125971765.10178,
+                   'Kprime_0': 4.38070845314276,
+                   'Kdprime_0': -2.834813574330987e-11,
+                   'a_0': 1.3063847563146157e-05,
+                   'T_einstein': 889.9701738863345,
+                   'H_Pref': -927677.8250983527,
+                   'S_Pref': 57.849598336365794,
+                   'Cp_Pref': np.array([1.17851662e+02, 3.67545477e-03,
+                                        -5.58729690e+06, 1.86118367e+02]),
+                   'molar_mass': 0.1003887,
+                   'n': 5.0,
+                   'formula': {'O': 3.0, 'Mg': 1.0, 'Si': 1.0}}
+
+
+water_params = {'name': 'water',
+                'equation_of_state': 'mod_hp_tmt',
+                'T_0': 298.15,
+                'Pref': 17500000000.0,
+                'V_0': 1.3230849810056659e-05,
+                'K_0': 23855375995.545673,
+                'Kprime_0': 3.1271291860824606,
+                'Kdprime_0': -5.950260351896275e-11,
+                'a_0': 0.00022967262033577235,
+                'T_einstein': 0.22907580091146804,
+                'H_Pref': -63479.066549524505,
+                'S_Pref': -28.056180880343014,
+                'Cp_Pref': np.array([7.31268765e+01,  1.14832240e-03,
+                                     1.91210537e+07, -1.02859341e+03]),
+                'molar_mass': 0.01801528, 'n': 3.0,
+                'formula': {'H': 2.0, 'O': 1.0}}
---
+++ contrib/water_filter/print_eos_table.py
@@ -0,0 +1,55 @@
+from model_parameters import mg_wadsleyite_params, fe_wadsleyite_params, majorite_params, water_params
+
+ordered_properties = [('name', 'name'),
+                      ('formula', 'formula'),
+                      ('n', 'n'),
+                      ('Pref', '$P\\textsubscript{ref} (Pa)$'),
+                      ('molar_mass', 'mass (kg/mol)'),
+                      ('T_0', '$T\\textsubscript{ref}$ (K)'),
+                      ('T_einstein', '$T\\textsubscript{ein}$ (K)'),
+                      ('H_Pref', '$\\mathcal{H}(P\\textsubscript{ref}, T\\textsubscript{ref})$ (J/mol)'),
+                      ('S_Pref', '$S(P\\textsubscript{ref}, T\\textsubscript{ref})$ (J/K/mol)'),
+                      ('V_0', '$V_0$ (m$^3$/mol)'),
+                      ('K_0', '$K_0$ (Pa)'),
+                      ('Kprime_0', "$K'_0$"),
+                      ('Kdprime_0', "$K''_0$ (/Pa)"),
+                      ('a_0', '$a_0$ (/K)'),
+                      ('Cp_Pref', ['J/K/mol', 'J/K$^2$/mol', 'JK/mol', 'JK$^{-1/2}$/mol'])]
+
+possible_elements = ['Fe', 'Mg', 'Si', 'H', 'O']
+
+mins = [mg_wadsleyite_params, fe_wadsleyite_params, majorite_params, water_params]
+print('\\hline')
+for (prp, prpname) in ordered_properties:
+    if prp != 'Cp_Pref' and prp != 'formula':
+        print(f'{prpname}', end='')
+        for m in mins:
+            try:
+                print(f' & {m[prp]:.6g}', end='')
+            except:
+                print(f' & {m[prp]}', end='')
+        print(' \\\\', end='\n')
+    elif prp == 'Cp_Pref':
+        for i in range(4):
+            print(f'$C_{i}$ ({prpname[i]})', end='')
+            for m in mins:
+                print(f' & {m[prp][i]:.4g}', end='')
+            print(' \\\\', end='\n')
+    else:
+        print(f'{prpname}', end='')
+        for m in mins:
+            print(' & \ce{', end='')
+            for el in possible_elements:
+                if el in m[prp]:
+                    nel = m[prp][el]
+                    if nel % 1 < 1.e-5:
+                        if nel > 1:
+                            print(f'{el}{int(nel)}', end='')
+                        else:
+                            print(f'{el}', end='')
+                    else:
+                        print(f'{el}_{{{nel}}}', end='')
+            print(f'}}', end='')
+        print('\\\\', end='\n')
+    if prp == 'name':
+        print('\\hline')
